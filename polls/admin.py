b7f2fcaa16ee8c6355840c7463052180b5f30965
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-24 11:13:29 +0300

    learning classes

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
-from .models import Manufacturer, Person,Runner,Library_Books,Fruit, car
+from .models import Group, Manufacturer, Membership, Person, Persona,Runner,Library_Books,Fruit, car
 
-admin.site.register(Person)
+admin.site.register(Persona)
 admin.site.register(Runner)
 admin.site.register(Library_Books)
 admin.site.register(Fruit)
 admin.site.register(Manufacturer)
 admin.site.register(car)
+admin.site.register(Person)
+admin.site.register(Group)
+admin.site.register(Membership)
 
 
 
--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,9 @@
+from faulthandler import dump_traceback_later
 from inspect import classify_class_attrs
 from tkinter import CASCADE
 from django.db import models
 
-class Person(models.Model):
+class Persona(models.Model):
     SHIRT_SIZES = (
         ("S", "Small"),
         ("M", "Medium"),
@@ -35,6 +36,24 @@ class Manufacturer(models.Model):
     pass
 class car(models.Model):
     manufacturer=models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+class Person(models.Model):
+    name=models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name=models.CharField(max_length=200)
+    members=models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self):
+        return self.name
+class Membership(models.Model):
+    person=models.ForeignKey(Person, on_delete=models.CASCADE)
+    group=models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined=models.DateField()
+    invite_reason=models.CharField(max_length=86)
 
 
 # Create your models here.
